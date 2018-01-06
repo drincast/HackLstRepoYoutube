da703c346b7b4688c0e05b2fcd32f9c23c9ed361
drincastX <drincast@gmail.com>
2018-01-06 15:17:15 -0500

    se deja código funcionalpara ejecutar desde la consola del navegador'

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # HackLstRepoYoutube
-Código javascript para crear una lista de reproducción de vídeos de youtube. 
+Código javascript para crear una lista de reproducción de vídeos de youtube.
 
-El código se aplicara inicialmente directamente en la consola del navegador, para realizar la funcionalidad de reproducción de videos 
+El código se aplicara inicialmente directamente en la consola del navegador, para realizar la funcionalidad de reproducción de videos
 a partir de una lista de urls
 
 Se modificara el código HTML de la pagina youtube, en el navegador, protegiendo todo el código original de la pagina en un iframe.
 
-Pasos para ejecutar el Hack
+Pasos para ejecutar el Hack, en los comentarios del código (hlry.js)
 
-1 - 
+* 2018-01-06 - versión 0.1
+  El código se ejecura por pasos en la consola de javascript del navegador, con previa carga de la pagina de yputube
+  con un video.
--- hlry.js
+++ hlry.js
@@ -1,16 +1,28 @@
-/***************************************************************************************************/
+/*
+  autor: drincastX (https://github.com/drincast)
+  Código javascript para simular una lista de reproducción de videos de youtube
+  versión: 0.1
+  2018-01-04
+*/
+
+// primero ejecutar lo siguiente hatas el comentario [FIN 1]
+//la variable lstVideos es el array donde se agregara la lista de videos
 var lstVideos = ['nada', 'https://www.youtube.com/watch?v=kkx-7fsiWgg', 'https://www.youtube.com/watch?v=YG2p6XBuSKA', 'https://www.youtube.com/watch?v=CRN75ev73JY'];
 var nodo = document.getElementById('content');
 var nodoPadre = nodo.parentNode;
 var iframeVideo = document.createElement('iframe');
 iframeVideo.setAttribute('id', 'iframeVideoX');
 iframeVideo.setAttribute('style', 'width:100%; height: 800px');
-iframeVideo.setAttribute('src', 'https://www.youtube.com/watch?v=OxxggwHFj7M');
+iframeVideo.setAttribute('src', 'https://www.youtube.com/watch?v=OxxggwHFj7M'); //la url puede ser cualquiera de un video de youtube
+// FIN 1
 
+// ejecutar de segunto lo siguiente hasta el comentario [FIN 2]
 //nodo.parentNode.children.content
 nodo.parentNode.removeChild(nodo);
 nodoPadre.appendChild(iframeVideo);
+// FIN 2
 
+//Ejecutar lo siguiente hasta el comentario [FIN 3]
 var varConteVideo = iframeVideo.contentDocument.getElementById('movie_player').getElementsByTagName('video');
 
 function ActulizarNodoVideo(){
@@ -20,10 +32,11 @@ function ActulizarNodoVideo(){
 
 function SiguienteVideo(){
   lstVideos.shift();
+  iframeVideo.setAttribute('src', lstVideos[0]);
   //document.location.target = "_self";
   //document.location.href=urlListaMusica[0];
   //document.location.href = sigVideoUrl;
-  iframeVideo.setAttribute('src', lstVideos[0]);
   setTimeout(ActulizarNodoVideo, 7000);
 }
 varConteVideo[0].onended = SiguienteVideo;
+// FIN 3
