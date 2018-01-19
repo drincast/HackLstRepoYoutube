c9acb591ff4ecdbe4ef95d3bcf58fa377c9d88d0
drincastX <drincast@gmail.com>
2018-01-19 10:25:14 -0500

    se limpia código, se deja la clase para el manejo de la lista

--- hlry.js
+++ hlry.js
@@ -45,175 +45,39 @@ varConteVideo[0].onended = SiguienteVideo;
 /************************************************************************************************************/
 //VERSIÓN CON CLASE PARA EL MANEJO DE LA LISTA DE REPRODUCCIÓN
 
-// primero ejecutar lo siguiente hatas el comentario [FIN 1]
-//la variable lstVideos es el array donde se agregara la lista de videos
-//var lstVideos = ['nada', 'https://www.youtube.com/watch?v=kkx-7fsiWgg', 'https://www.youtube.com/watch?v=YG2p6XBuSKA', 'https://www.youtube.com/watch?v=CRN75ev73JY'];
-var nodo = document.getElementById('content');
-var nodoPadre = nodo.parentNode;
-var iframeVideo = document.createElement('iframe');
-iframeVideo.setAttribute('id', 'iframeVideoX');
-iframeVideo.setAttribute('style', 'width:100%; height: 800px');
-iframeVideo.setAttribute('src', 'https://www.youtube.com/watch?v=fciL4yULkns'); //la url puede ser cualquiera de un video de youtube
-// FIN 1
-
-//Se inicia creación de clase/objeto que maneje la reproducción
-hlry = {
-  p: {
-    lstVideos: [],
-    indiceVideoActual: 0,
-  },
-
-  m: {
-    agregarVideoAlFinal: function(url){
-      hlry.p.lstVideos.push(url);
-    },
-
-    agregarVideoAlInicio: function(url){
-      hlry.p.lstVideos.unshift(url);
-    },
-
-    agregarVideoDespues: function(url){
-      hlry.p.lstVideos.splice(1, 0, url);
-    },
-
-    agregarVideoAlInicio2: function(){
-      if(txtUrlVideoYoutube.value !== ""){
-        console.log(txtUrlVideoYoutube.value);
-        hlry.p.lstVideos.unshift(url);
-        txtUrlVideoYoutube.value = "";
-      }
-    },
-
-    agregarVideoDespues2: function(){
-      if(txtUrlVideoYoutube.value !== ""){
-        console.log(txtUrlVideoYoutube.value);
-        hlry.p.lstVideos.splice(1, 0, txtUrlVideoYoutube.value);
-        txtUrlVideoYoutube.value = "";
-      }
-    },
-
-    siguiente: function(){
-      SiguienteVideo();
-    }
-  }
-};
-
-
-
-// ejecutar de segunto lo siguiente hasta el comentario [FIN 2]
-//nodo.parentNode.children.content
-nodo.parentNode.removeChild(nodo);
-nodoPadre.appendChild(iframeVideo);
-// FIN 2
-
-//Ejecutar lo siguiente hasta el comentario [FIN 3]
-var varConteVideo = iframeVideo.contentDocument.getElementById('movie_player').getElementsByTagName('video');
-
-function ActulizarNodoVideo(){
-  varConteVideo = iframeVideo.contentDocument.getElementById('movie_player').getElementsByTagName('video');
-  varConteVideo[0].onended = SiguienteVideo;
-}
-
-//se modifica para que sea un ciclo infinito
-function SiguienteVideo(){
-  hlry.p.lstVideos.push(hlry.p.lstVideos[0]);
-  hlry.p.lstVideos.shift();
-  iframeVideo.setAttribute('src',hlry.p.lstVideos[0]);
-  //document.location.target = "_self";
-  //document.location.href=urlListaMusica[0];
-  //document.location.href = sigVideoUrl;
-  setTimeout(ActulizarNodoVideo, 7000);
-}
-varConteVideo[0].onended = SiguienteVideo;
-// FIN 3
-
-// var iframe2 = document.getElementById('iframeVideoX');
-// iframe2.contentDocument.getElementById('container').style.display = "none";
-
-var divReproductor = document.createElement('div');
-divReproductor.setAttribute('id', 'divReproductor');
-divReproductor.style.display = 'block';
-
-var divSiguiente = document.createElement('div');
-divSiguiente.style.marginBottom = '5px';
-var btnSiguiente = document.createElement('button');
-btnSiguiente.setAttribute('id', 'btnSiguiente');
-btnSiguiente.innerHTML = 'Siguiente';
-btnSiguiente.addEventListener('click', SiguienteVideo);
-divSiguiente.appendChild(btnSiguiente);
-
-var divURL = document.createElement('div');
-divURL.style.marginBottom = '5px';
-var lblURLV = document.createElement('label');
-lblURLV.setAttribute('for', 'txtUrlVideoYoutube');
-lblURLV.innerHTML = 'URL Video: ';
-var txtUrlVideoYoutube = document.createElement('input');
-txtUrlVideoYoutube.setAttribute('id', 'txtUrlVideoYoutube');
-divURL.appendChild(lblURLV);
-divURL.appendChild(txtUrlVideoYoutube);
-
-var divCola = document.createElement('div');
-divCola.style.marginBottom = '5px';
-var btnJDespues = document.createElement('button');
-var btnCola = document.createElement('button');
-btnJDespues.setAttribute('id', 'btnJDespues');
-btnCola.setAttribute('id', 'btnCola');
-btnJDespues.innerHTML = 'justo despues';
-btnCola.innerHTML = 'al final';
-// btnJDespues.addEventListener('onclick', hlry.m.agregarVideoDespues("'"+txtUrlVideoYoutube.value+"'"));
-// btnCola.addEventListener('onclick', hlry.m.agregarVideoAlFinal("'"+txtUrlVideoYoutube.value+"'"));
-btnJDespues.addEventListener('click', hlry.m.agregarVideoDespues2);
-btnCola.addEventListener('click', hlry.m.agregarVideoAlFinal2);
-divCola.appendChild(btnJDespues);
-divCola.appendChild(btnCola);
-
-divReproductor.appendChild(divSiguiente);
-divReproductor.appendChild(divURL);
-divReproductor.appendChild(divCola);
-
-var appYT = document.getElementsByTagName('ytd-app')[0];
-appYT.style.position = 'inherit';
-//var container = document.getElementById('container');
-
-document.getElementsByTagName('body')[0].insertBefore(divReproductor, appYT);
-
-///
-
-
-// var btnAgregar = document.createElement('buttom');
-// btnAgregar.setAttribute('id', 'btnAgregar');
-// btnAgregar.innerHTML = 'Agregar';
-// divControles.appendChild(txtUrlVideoYoutube);
-// divControles.appendChild(btnAgregar);
-
-
-
-
-
-
-
-
+//listas de videos
 //salsa
-lstVideos: ["https://www.youtube.com/watch?v=AYc8SC5nnNA", "https://www.youtube.com/watch?v=wU52X9duq6U", "https://www.youtube.com/watch?v=hQdN437xMHg", "https://www.youtube.com/watch?v=scC8I9xl4eg", "https://www.youtube.com/watch?v=cgXGhyGwMFc", "https://www.youtube.com/watch?v=7z2SULKMdiQ", "https://www.youtube.com/watch?v=JfoIqtyCnCU", "https://www.youtube.com/watch?v=7KxkMLAZlzw", "https://www.youtube.com/watch?v=J1--Eg5DPRo", "https://www.youtube.com/watch?v=arWvQNcLKqs", "https://www.youtube.com/watch?v=A3uIUXMJ1xc", "https://www.youtube.com/watch?v=KS9xeYh-HWk", "https://www.youtube.com/watch?v=XiEoCGjwN08", "https://www.youtube.com/watch?v=r4WWa5j-NJk", "https://www.youtube.com/watch?v=WI0Ikrs3E_k", "https://www.youtube.com/watch?v=ZChfr2rhQEg", "https://www.youtube.com/watch?v=q9SqG4XX8ZU", "https://www.youtube.com/watch?v=7vuYeJ1gSSE", "https://www.youtube.com/watch?v=x6EGixAn7Lk", "https://www.youtube.com/watch?v=3H2rE2i_fL4", "https://www.youtube.com/watch?v=0wWRdgEw1I0", "https://www.youtube.com/watch?v=XixGgMfaJck", "https://www.youtube.com/watch?v=RGqQXW_qblg", "https://www.youtube.com/watch?v=1ZguRilbRjA", "https://www.youtube.com/watch?v=83h-cjtfTgE", "https://www.youtube.com/watch?v=coW-lY_uHWM", "https://www.youtube.com/watch?v=QkZscosb6mM", "https://www.youtube.com/watch?v=hd0cl2d2ziU", "https://www.youtube.com/watch?v=Njkg2ubUaug", "https://www.youtube.com/watch?v=fciL4yULkns", "https://www.youtube.com/watch?v=k4ZYi0DbpTY", "https://www.youtube.com/watch?v=JfoIqtyCnCU"],
+["AYc8SC5nnNA", "wU52X9duq6U", "hQdN437xMHg", "scC8I9xl4eg", "cgXGhyGwMFc", "7z2SULKMdiQ", "JfoIqtyCnCU", "7KxkMLAZlzw", "J1--Eg5DPRo", "arWvQNcLKqs", "A3uIUXMJ1xc", "KS9xeYh-HWk", "XiEoCGjwN08", "r4WWa5j-NJk", "WI0Ikrs3E_k", "ZChfr2rhQEg", "q9SqG4XX8ZU", "7vuYeJ1gSSE", "x6EGixAn7Lk", "3H2rE2i_fL4", "0wWRdgEw1I0", "XixGgMfaJck", "RGqQXW_qblg", "1ZguRilbRjA", "83h-cjtfTgE", "coW-lY_uHWM", "QkZscosb6mM", "hd0cl2d2ziU", "Njkg2ubUaug", "fciL4yULkns", "k4ZYi0DbpTY", "JfoIqtyCnCU"]
 
 //rock
-lstVideos: ["https://www.youtube.com/watch?v=EU0LljxpHIk", "https://www.youtube.com/watch?v=fnD0MKzq6QI", "https://www.youtube.com/watch?v=6NXnxTNIWkc", "https://www.youtube.com/watch?v=75_slEkywpY", "https://www.youtube.com/watch?v=hSylsKEKGCo", "https://www.youtube.com/watch?v=z_NJhmtQWAs", "https://www.youtube.com/watch?v=PUzWDf9S9Rk", "https://www.youtube.com/watch?v=JAU88rTbPGg", "https://www.youtube.com/watch?v=fciL4yULkns", "https://www.youtube.com/watch?v=r2g0pM3PMNQ", "https://www.youtube.com/watch?v=1w7OgIMMRc4", "https://www.youtube.com/watch?v=l9kXym1doYA", "https://www.youtube.com/watch?v=twEGSGpLiyc", "https://www.youtube.com/watch?v=YbK00KvsNAE", "https://www.youtube.com/watch?v=re1btP_rGys", "https://www.youtube.com/watch?v=7QU1nvuxaMA"],
+["EU0LljxpHIk", "fnD0MKzq6QI", "6NXnxTNIWkc", "75_slEkywpY", "hSylsKEKGCo", "z_NJhmtQWAs", "PUzWDf9S9Rk", "JAU88rTbPGg", "fciL4yULkns", "r2g0pM3PMNQ", "1w7OgIMMRc4", "l9kXym1doYA", "twEGSGpLiyc", "YbK00KvsNAE", "re1btP_rGys", "7QU1nvuxaMA"]
 
 //electro
-lstVideos: ["https://www.youtube.com/watch?v=JRfuAukYTKg", "https://www.youtube.com/watch?v=N9hazmsUxrM", "https://www.youtube.com/watch?v=gCYcHz2k5x0", "https://www.youtube.com/watch?v=JwQZQygg3Lk", "https://www.youtube.com/watch?v=PVzljDmoPVs", "https://www.youtube.com/watch?v=ALZHF5UqnU4", "https://www.youtube.com/watch?v=_ovdm2yX4MA", "https://www.youtube.com/watch?v=hWjrMTWXH28", "https://www.youtube.com/watch?v=EVr__5Addjw", "https://www.youtube.com/watch?v=jUe8uoKdHao", "https://www.youtube.com/watch?v=mIIN_SGQy9c", "https://www.youtube.com/watch?v=ebXbLfLACGM", "https://www.youtube.com/watch?v=hHUbLv4ThOo", "https://www.youtube.com/watch?v=IcrbM1l_BoI", "https://www.youtube.com/watch?v=u0pmV7GoTjc", "https://www.youtube.com/watch?v=NUVCQXMUVnI", "https://www.youtube.com/watch?v=EPo5wWmKEaI", "https://www.youtube.com/watch?v=u9LH_y159sg", "https://www.youtube.com/watch?v=YxIiPLVR6NA", "https://www.youtube.com/watch?v=5jlI4uzZGjU", "https://www.youtube.com/watch?v=TSNerxNwWtU", "https://www.youtube.com/watch?v=nYh-n7EOtMA", "https://www.youtube.com/watch?v=cHHLHGNpCSA", "https://www.youtube.com/watch?v=HJb0VYVtaNc", "https://www.youtube.com/watch?v=BR_DFMUzX4E", "https://www.youtube.com/watch?v=b4Bj7Zb-YD4", "https://www.youtube.com/watch?v=fciL4yULkns", "https://www.youtube.com/watch?v=5dbEhBKGOtY"]
+["JRfuAukYTKg", "N9hazmsUxrM", "gCYcHz2k5x0", "JwQZQygg3Lk", "PVzljDmoPVs", "ALZHF5UqnU4", "_ovdm2yX4MA", "hWjrMTWXH28", "EVr__5Addjw", "jUe8uoKdHao", "mIIN_SGQy9c", "ebXbLfLACGM", "hHUbLv4ThOo", "IcrbM1l_BoI", "u0pmV7GoTjc", "NUVCQXMUVnI", "EPo5wWmKEaI", "u9LH_y159sg", "YxIiPLVR6NA", "5jlI4uzZGjU", "TSNerxNwWtU", "nYh-n7EOtMA", "cHHLHGNpCSA", "HJb0VYVtaNc", "BR_DFMUzX4E", "b4Bj7Zb-YD4", "fciL4yULkns", "5dbEhBKGOtY"]
 
 //varios
-["https://www.youtube.com/watch?v=7zp1TbLFPp8", "https://www.youtube.com/watch?v=kXYiU_JCYtU", "https://www.youtube.com/watch?v=GurkREc-q4I", "https://www.youtube.com/watch?v=H2jCbXiEQI4", "https://www.youtube.com/watch?v=6dW6aNAZGTM", "https://www.youtube.com/watch?v=p-Z3YrHJ1sU", "https://www.youtube.com/watch?v=RYnFIRc0k6E", "https://www.youtube.com/watch?v=mIIN_SGQy9c", "https://www.youtube.com/watch?v=m3fx4LUBSrM", "https://www.youtube.com/watch?v=gmc4tkVJow8", "https://www.youtube.com/watch?v=EPo5wWmKEaI", "https://www.youtube.com/watch?v=E3zxvWvxDjQ", "https://www.youtube.com/watch?v=mcGBVy3-W4s", "https://www.youtube.com/watch?v=2up_Eq6r6Ko", "https://www.youtube.com/watch?v=9J9FlVCUeLM", "https://www.youtube.com/watch?v=5jlI4uzZGjU", "https://www.youtube.com/watch?v=EVr__5Addjw", "https://www.youtube.com/watch?v=j0lSpNtjPM8", "https://www.youtube.com/watch?v=S5FCdx7Dn0o", "https://www.youtube.com/watch?v=cHHLHGNpCSA", "https://www.youtube.com/watch?v=EH6myRjE848", "https://www.youtube.com/watch?v=jzy2dgEUOhY", "https://www.youtube.com/watch?v=CdXesX6mYUE", "https://www.youtube.com/watch?v=5ZF5m-vmDzM", "https://www.youtube.com/watch?v=jRGrNDV2mKc", "https://www.youtube.com/watch?v=7iNbnineUCI", "https://www.youtube.com/watch?v=0pPXOq87atY", "https://www.youtube.com/watch?v=VqEbCxg2bNI", "https://www.youtube.com/watch?v=_i2EwTKRmsU", "https://www.youtube.com/watch?v=920jLVQOLFI", "https://www.youtube.com/watch?v=YbK00KvsNAE", "https://www.youtube.com/watch?v=SHq2qrFUlGY", "https://www.youtube.com/watch?v=0S3foICf5uI", "https://www.youtube.com/watch?v=OxxggwHFj7M", "https://www.youtube.com/watch?v=_I00wIWw7FQ", "https://www.youtube.com/watch?v=RIUy6aCqUMw", "https://www.youtube.com/watch?v=jnDjEHyhFpU", "https://www.youtube.com/watch?v=CHekNnySAfM", "https://www.youtube.com/watch?v=fciL4yULkns", "https://www.youtube.com/watch?v=YG2p6XBuSKA", "https://www.youtube.com/watch?v=SmM0653YvXU", "https://www.youtube.com/watch?v=5dbEhBKGOtY"]
+["7zp1TbLFPp8", "kXYiU_JCYtU", "GurkREc-q4I", "H2jCbXiEQI4", "6dW6aNAZGTM", "p-Z3YrHJ1sU", "RYnFIRc0k6E", "mIIN_SGQy9c", "m3fx4LUBSrM", "gmc4tkVJow8", "EPo5wWmKEaI", "E3zxvWvxDjQ", "mcGBVy3-W4s", "2up_Eq6r6Ko", "9J9FlVCUeLM", "5jlI4uzZGjU", "EVr__5Addjw", "j0lSpNtjPM8", "S5FCdx7Dn0o", "cHHLHGNpCSA", "EH6myRjE848", "jzy2dgEUOhY", "CdXesX6mYUE", "5ZF5m-vmDzM", "jRGrNDV2mKc", "7iNbnineUCI", "0pPXOq87atY", "VqEbCxg2bNI", "_i2EwTKRmsU", "920jLVQOLFI", "YbK00KvsNAE", "SHq2qrFUlGY", "0S3foICf5uI", "OxxggwHFj7M", "_I00wIWw7FQ", "RIUy6aCqUMw", "jnDjEHyhFpU", "CHekNnySAfM", "fciL4yULkns", "YG2p6XBuSKA", "SmM0653YvXU", "5dbEhBKGOtY"]
 
 //piano
-["https://www.youtube.com/watch?v=JbjzPKTfjlc", "https://www.youtube.com/watch?v=rExcQ5nm_yU", "https://www.youtube.com/watch?v=Pm1a7VzISK4", "https://www.youtube.com/watch?v=pL0bxewHbjo", "https://www.youtube.com/watch?v=2W_G3xmSGfo", "https://www.youtube.com/watch?v=W_0GdB4oD6s", "https://www.youtube.com/watch?v=4I_NYya-WWg", "https://www.youtube.com/watch?v=GdU6snztM0A", "https://www.youtube.com/watch?v=eHwR9kTIMeI", "https://www.youtube.com/watch?v=NvryolGa19A", "https://www.youtube.com/watch?v=kcihcYEOeic", "https://www.youtube.com/watch?v=fciL4yULkns"]
+["JbjzPKTfjlc", "rExcQ5nm_yU", "Pm1a7VzISK4", "pL0bxewHbjo", "2W_G3xmSGfo", "W_0GdB4oD6s", "4I_NYya-WWg", "GdU6snztM0A", "eHwR9kTIMeI", "NvryolGa19A", "kcihcYEOeic", "fciL4yULkns"]
 
 
 //lista rock español
-["iQiQYbTA0sw", "wL2012Q3h3M", "75_slEkywpY", "YbK00KvsNAE", "zABYI-f7wkE", "re1btP_rGys", "AruE3oCxF70", "UJqcH0my0mY", "uvVNoP6U1OQ", "50jlKbvk9O0", "slctnzJ2dzA", "yh8S4Blnd6M", "Ru24puGqKuM", "hSylsKEKGCo", "uZ3x0I5lIGo", "_R-l7KYx9sM", "HIppnw4wt7Q", "fnD0MKzq6QI", "hbiQn4-1qv8", "PUzWDf9S9Rk", "R7s6rZuuff8", "guHN79sQ3gk", "Kdp4KAyO2Zo", "BESD2Oo2PrQ"]
+["UJqcH0my0mY", "uvVNoP6U1OQ", "50jlKbvk9O0", "slctnzJ2dzA", "yh8S4Blnd6M", "Ru24puGqKuM", "hSylsKEKGCo", "uZ3x0I5lIGo", "_R-l7KYx9sM", "HIppnw4wt7Q", "fnD0MKzq6QI", "hbiQn4-1qv8", "PUzWDf9S9Rk", "R7s6rZuuff8", "guHN79sQ3gk", "Kdp4KAyO2Zo", "BESD2Oo2PrQ", "iQiQYbTA0sw", "wL2012Q3h3M", "CJcr4Yg9DMA", "EU0LljxpHIk", "75_slEkywpY", "YbK00KvsNAE", "zABYI-f7wkE", "lEa1F3wyuho", "re1btP_rGys", "AruE3oCxF70", "5k-QLyo9Hoc"]
 
+
+https://www.youtube.com/watch?v=y9uqsn33MS4
 
 //---------------------------------------------------------
+//guia: copiar el código acontinuación y copiarlo en la consola del browser
+//luego Ejecutar la siguiente lines
+//
+//hlry.m.iniciar()
+//luego ejecutar la siguiente linea
+//hlry.crearcontrolesHTML()
+//con esto se crean unos controles html al inicio de la pagina, con lo cual, se puede
+//cambiar al siguiente video, y agregar nuevos videos a la lista
+
 
 //Se inicia creación de clase/objeto que maneje la reproducción
 hlry = {};
@@ -366,5 +230,15 @@ hlry.m = {
     //var container = document.getElementById('container');
 
     document.getElementsByTagName('body')[0].insertBefore(hlry.p.divReproductor, hlry.p.appYT);
+  },
+
+  limpiararrayUrl: function(array){
+    if(array !== null || array !== undefined){
+      array.forEach(function(item, index){
+        array[index] = item.replace(hlry.p.urlPpal, '');
+
+      });
+    }
+    return array;
   }
 };
